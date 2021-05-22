6d69f5de1edf2b4274414312168638e00e4933d8
TungCheng <ga2006088445@gmail.com>
2021-05-22 13:48:37 +0800

    (^) 整理流程

--- src/main.go
+++ src/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"os"
-
 	"github.com/labstack/gommon/log"
 )
 
 func main() {
-	arg := os.Args[1]
-	if arg == "debug" {
-		screenDebug()
+
+	for {
+		succ := checkMainScreen()
+		if succ {
+			taskFeatures()
+		} else {
+			taskLogin()
+		}
 	}
+}
+
+func taskLogin() {
+	// joindesktop()
+	// closeblack()
 
 	infoConfig, err := LoadConfigInfo()
 	if err != nil {
 		log.Errorf("cannot load config:", err)
 	}
 	openGameClient(infoConfig.GamePath)
-	// accConfig, err := LoadConfigAccount()
-	// if err != nil {
-	// 	log.Errorf("cannot load config:", err)
-	// }
-
-	// setAccPW(accConfig.Account, accConfig.Password)
+	accConfig, err := LoadConfigAccount()
+	if err != nil {
+		log.Errorf("cannot load config:", err)
+	}
+	setAccPW(accConfig.Account, accConfig.Password)
 
 	// pwd, time := load2FA(accConfig.FAkey)
 
 	// join()
+}
+
+func taskFeatures() {
+
+}
+
+func checkMainScreen() (ok bool) {
+	return false
 }
---
+++ src/setAccPW.go
@@ -0,0 +1,5 @@
+package main
+
+func setAccPW(acc string, pw string) {
+
+}
