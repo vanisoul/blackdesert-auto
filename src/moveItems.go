a256fb4abb1a9ba1dbdbede6d3d4fc6a465f6e93
TungCheng <ga2006088445@gmail.com>
2021-05-30 15:13:55 +0800

    (^) 釋放圖片空間

--- src/moveItems.go
+++ src/moveItems.go
@@ -118,6 +118,7 @@ func checkFullWeight(img string) (succ bool) {
 	robotgo.MoveMouse(0, 0)
 	robotgo.Sleep(1)
 	bit := robotgo.CaptureScreen(1400, 250, 500, 600)
+	defer robotgo.FreeBitmap(bit)
 	succ = screenYesOrNoDotSelfimg(insertStrToFilenameTail(img, "bank"), bit, 0.03, 2)
 	return
 }
